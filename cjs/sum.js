255c51032a895c8db51650fb97cef18402d8e18d
jtrujillo95 <jtrujillormz@outlook.com>
2025-03-21 02:40:39 -0600

    CommonJs Modules import/export

---
+++ cjs/sum.js
@@ -0,0 +1,9 @@
+function sum (a,b) {
+  return a + b
+}
+
+// Se obliga al archivo donde se importe, a que tenga el mismo nombre que nuestra función
+// CommonJS Module export
+module.exports = {
+  sum
+}
